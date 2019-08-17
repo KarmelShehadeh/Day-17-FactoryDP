ceed11aff4beb5ce98c2ba83a67060adb62ca7a2
karmel shehadeh <karmel.1195@gmail.com>
2019-08-17 20:16:53 +0300

    Use FileFactory class to get object of concrete class by passing the type in the maim class

--- src/main.java
+++ src/main.java
@@ -10,6 +10,19 @@ Created by karmel shehadeh
 
 public class main {
     public static void main(String[] arguments) {
+        FileFactory fileFactory = new FileFactory();
+
+        //get an object of xmlFile and call its pars method.
+        FileParser xmlFile = fileFactory.getType("XML");
+
+        //call draw method of Circle
+        xmlFile.pars();
+
+        //get an object of jsonFile and call its pars method.
+        FileParser jsonFile = fileFactory.getType("Json");
+
+        //call draw method of Rectangle
+        jsonFile.pars();
 
 
     }
